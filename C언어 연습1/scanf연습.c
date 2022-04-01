a87a31f355b433d9df0708670cfa40a16384eb11
jaeyong <apdls155@naver.com>
2022-04-01 15:35:53 +0900

    연습

---
+++ C언어 연습1/scanf연습.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+//	int a;     // int형으로 가져오기 
+//	printf("제일 좋아하는 숫자는 무엇입니까?");
+//	scanf("%d",&a);
+//	printf("\n제가 제일 좋아하는 숫자는 %d입니다.",a);
+
+//	float b;   // float 형으로 가져오기 
+//	printf("당신이 제일  좋아하는 실수는 무엇입니까?");
+//	scanf("%f",&b);
+//	printf("\n제가 제일 좋아하는 실수는 %f입니다.",b);
+
+//	char a[10];  // 문자 가져오기 
+//	printf("당신이 제일 좋아하는 문구는 무엇입니까?");
+//	scanf("%s",&a);
+//	printf("\n제가 제일 좋아하는 문구는 %s입니다.",a);
+	
+	int a;
+	printf("입력:");
+	scanf("%d",&a);
+	printf("\n결과값:%d",a); 
+	
+	return 0;
+}
